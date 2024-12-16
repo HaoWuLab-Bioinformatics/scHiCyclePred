b9f6e71b47ae5e68148ace346e2467001dc35ad3
HaoWuLab-Bioinformatics <55370215+HaoWuLab-Bioinformatics@users.noreply.github.com>
2024-12-16 10:00:20 +0800

    Update method_whole_train_set.py

--- BCP/test_model/method_whole_train_set.py
+++ BCP/test_model/method_whole_train_set.py
@@ -1,14 +1,14 @@
-from collections import Counter
+import os, random
 
 import pandas as pd
-import xlsxwriter
-from scipy import stats
 import numpy as np
-from method_whole_train_set import load_BCP_dict, CNN_1D_montage
-from sklearn.model_selection import train_test_split, StratifiedKFold
-import random,os, torch
+from methods import generate_bin, replace_linetodot
+from torch.utils.data import DataLoader, TensorDataset
+import torch
+import torch.nn as nn
+from pytorch_tools import EarlyStopping
 
-#BCP特征集对应的网络的测试
+from torch.optim import Adam
 
 def seed_torch(seed=2021):
     random.seed(seed)
@@ -19,43 +19,165 @@ def seed_torch(seed=2021):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+def load_BCP_dict():
+    file_path = "./Data/BCP_Nor.npy"
+    Data = np.load(file_path, allow_pickle=True).item()
+    return Data
 
-def main():
-    path = "./Data/new_cell_inf.txt"
-    cell_inf = pd.read_table(path, sep=' ', header='infer', names=None, index_col=None, dtype=None, engine=None,
-                             nrows=None)
-    cell_inf = cell_inf.sort_values(by='cycle', ascending=True)
-    X_index = np.array(cell_inf.values.tolist())[:, 0:1]
-    Y = []
-    label = np.array(cell_inf.values.tolist())[:, 1:]
-    for l in label:
-        if l == 'G1':
-            Y.append(0)
-        elif l == "early_S":
-            Y.append(1)
-        elif l == "mid_S":
-            Y.append(2)
-        elif l == "late_S":
-            Y.append(3)
-    Y = np.array(Y)
-    BCP = load_BCP_dict()
-    Con_layer = 2
-    linear_layer = 1
-    X_train, X_test, y_train, y_test = train_test_split(X_index, Y, test_size=0.2, random_state=4096, stratify=Y)
-    print("预测结果：", Counter(y_test))
-    kernel_size = 7
-    cnn_feature = 16
-    out_feature = 0
-    dp = 0.2
-    lr = 0.001
-    model_para = [kernel_size, cnn_feature, dp, out_feature, Con_layer, linear_layer]
-    test_acc, test_label, real_label = CNN_1D_montage(BCP, X_train, y_train, X_test, y_test, lr, model_para)
-    print('test_acc: ', test_acc)
-    label_count = []
-    for i, j in zip(test_label, real_label):
-        if i == j:
-            label_count.append(i)
-    print("预测结果：", Counter(label_count))
+def load_BCP_data(BCP, idX, Y):
+    # generate_bin()函数用于返回一个字典，字典中存的是每条染色体能够切出的块的数目，形如：
+    # {'chr1': 198, 'chr2': 183, .... 'chrX': 172, 'chrY': 92}
+    index = generate_bin()
+    chr_list = sorted(index.keys())
+    # print('BCP',idX[:5])
+    X = []
+    for cell in idX:
+        cell_name = replace_linetodot(cell[0]) + "_reads"
+        bcp = []
+        for chr in chr_list:
+            # 不考虑Y染色体的接触信息
+            if chr == "chrY":
+                continue
+            bcp.append(BCP[cell_name][chr])
+        X.append(np.concatenate(bcp).tolist())
+    # print(len(X))输出749。X是一个列表，其中的每一个元素也是一个一维列表，一个一维列表存的是一个细胞的全部bcp特征。
+    # print(np.array(X).shape)  # 如果输出(748, 2660)，也就是说当前的idX集合共有748个细胞，每个细胞对应一个包含有2660个特征数据的列表。一个细胞就对应一行数据
+    # print('BCP', X[:1])
+    # torch.from_numpy()方法把数组转换成张量
+    # 与torch.tensor()的区别：使用torch.from_numpy更加安全，使用tensor.Tensor在非float类型下会与预期不符。
+    deal_dataset = TensorDataset(torch.from_numpy(np.array(X).astype(float)), torch.from_numpy(np.array(Y).astype(int)))
+    return deal_dataset, np.array(X).shape[0]
+    # np.array(X).shape[0]返回的是当前当前的idX集合中细胞的个数
 
-if __name__ == '__main__':
-    main()
+
+
+class montage_model(nn.Module):
+    def __init__(self, model_para):
+        super(montage_model, self).__init__()
+        # (652-5+2)/1 + 1 = 652
+        kernel_size, cnn_feature, dp, out_feature, Con_layer, linear_layer = model_para
+        self.linear_layer = linear_layer
+        linear_size = 2660
+        for i in range(Con_layer):
+            linear_size = int(((linear_size+2*1-kernel_size)/1+1)//2)
+        self.linear_size = linear_size
+        self.cnn_feature = cnn_feature
+        self.conv1 = nn.Conv1d(
+            in_channels=1,
+            out_channels=cnn_feature,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=1
+        )
+        self.bn1 = nn.BatchNorm1d(num_features=cnn_feature)
+        self.rule1 = nn.ReLU()
+        self.pool = nn.MaxPool1d(kernel_size=2)
+        self.dropout = nn.Dropout(dp)
+        self.Con = nn.Sequential()
+
+        for i in range(Con_layer-1):
+            layer_id = str(i+2)
+            self.Con.add_module("conv%s" % layer_id,nn.Conv1d(in_channels=cnn_feature,out_channels=cnn_feature,kernel_size=kernel_size,stride=1,padding=1))
+            self.Con.add_module("bach%s" % layer_id,nn.BatchNorm1d(num_features=cnn_feature))
+            self.Con.add_module("relu%s" % layer_id,nn.ReLU())
+            self.Con.add_module("maxp%s" % layer_id,nn.MaxPool1d(kernel_size=2))
+            self.Con.add_module("drop%s" % layer_id,nn.Dropout(dp))
+
+        if linear_layer == 1:
+            self.fc = nn.Linear(in_features=cnn_feature * linear_size, out_features=4)
+        else:
+            self.fc1 = nn.Linear(in_features=cnn_feature * linear_size, out_features=out_feature)
+            self.relu2 = nn.ReLU()
+            self.Linear = nn.Sequential()
+            for i in range(linear_layer-2):
+                l_layer_id = str(i + 2)
+                self.Linear.add_module("linear%s" % l_layer_id,nn.Linear(in_features=out_feature, out_features=out_feature))
+                self.Linear.add_module("linear_relu%s" % l_layer_id,nn.ReLU())
+                self.Linear.add_module("linear_dropout%s" % l_layer_id, nn.Dropout(dp))
+            self.fc2 = nn.Linear(in_features=out_feature, out_features=4)
+
+        # Decay:488  Inscore:489
+
+
+    def forward(self, x):
+        x = self.rule1(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+        x = self.dropout(x)
+        x = self.Con(x)
+
+        x = x.view(-1, self.cnn_feature  * self.linear_size)
+        if self.linear_layer == 1:
+            x = self.fc(x)
+        else:# Decay:492  Inscore:489
+            x = self.fc1(x)
+            x = self.dropout(x)
+            x = self.relu2(x)
+            x = self.Linear(x)
+            x = self.fc2(x)
+        x = nn.functional.log_softmax(x, dim=1)
+        return x
+
+def CNN_train(epoch, model, optimizer, train_loader, loss_fn):
+    for i, (images, labels) in enumerate(train_loader):
+        optimizer.zero_grad()
+        labels = torch.Tensor(labels.type(torch.FloatTensor)).long()
+        images = images.type(torch.FloatTensor)
+        images = torch.unsqueeze(images, dim=1)
+        # images = images.to(device)
+        # labels = labels.to(device)
+        outputs = model(images)
+        train_loss = loss_fn(outputs, labels)
+        train_loss.backward()
+        optimizer.step()
+        train_loss += train_loss.cpu().data * images.size(0)
+        _, prediction = torch.max(outputs.data, 1)
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, i * len(images), len(train_loader.dataset),
+                   100. * i / len(train_loader), train_loss.cpu().data * images.size(0)))
+    return model, optimizer, train_loader
+
+def CNN_val(epoch,model, test_loader, loss_fn, test_size):
+    for i, (images, labels) in enumerate(test_loader):
+        images = images.type(torch.FloatTensor)
+        images = torch.unsqueeze(images, dim=1)
+        # images = images.to(device)
+        # labels = labels.to(device)
+        labels = torch.Tensor(labels.type(torch.FloatTensor)).long()
+        outputs = model(images)
+        val_loss = loss_fn(outputs, labels)
+        _, prediction = torch.max(outputs.data, 1)
+        label_pred = prediction.cpu().numpy()
+        label = labels.data.cpu().numpy()
+        prediction_num = int(torch.sum(prediction == labels.data))
+        val_accuracy = prediction_num / test_size
+        print('Val Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, i * len(images), len(test_loader.dataset),
+                   100. * i / len(test_loader), val_loss.cpu().data * images.size(0)))
+    return label_pred, label, val_loss, model, val_accuracy
+
+
+def CNN_1D_montage(BCP, tr_x, tr_y, te_x, te_y,lr,model_para):
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    seed_torch()
+    train_dataset, train_size = load_BCP_data(BCP, tr_x, tr_y)
+    test_dataset, test_size = load_BCP_data(BCP, te_x, te_y)
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=64,
+                              shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset,
+                            batch_size=test_size,
+                            shuffle=False)
+    model = montage_model(model_para)
+    num_epochs = 100
+    optimizer = Adam(model.parameters(), lr=lr)
+    loss_fn = nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):
+        model.train()
+        model, optimizer, train_loader = CNN_train(epoch, model, optimizer, train_loader, loss_fn)
+        model.eval()
+        test_label, label, test_loss, model, test_accuracy = CNN_val(epoch, model, test_loader,
+                                                                          loss_fn, test_size)
+    torch.cuda.empty_cache()
+
+    return test_accuracy, test_label, label
+
